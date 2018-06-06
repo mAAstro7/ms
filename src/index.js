60f749eb648cfd9592fda7a95c1edc9a9c7b610f
Joonatan <joonatan.vuorela@gmail.com>
2018-06-06 14:39:35 +0300

    working simple form1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Values } from "redux-form-website-template";
+import store from "./store";
+import showResults from "./showResults";
+import MS from './MoneySaver';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootEl = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div style={{ padding: 15 }}>
+      <h2>Simple Form</h2>
+      <App/>
+      <Values form="ms" />
+    </div>
+  </Provider>,
+  rootEl
+);
